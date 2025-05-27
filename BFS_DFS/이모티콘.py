19ea1fa0f77226f8c793234fdfe1898204093ca7
jusepv <jinjs0704@gmail.com>
2025-05-27 11:58:27 +0900

    250527

---
+++ BFS_DFS/이모티콘.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+S = int(input())
+
+
+q = deque()
+q.append((1,0)) # (화면에 존재하는 이모티콘 수, 클립보드에 저장된 이모티콘 수)
+visited = [[0]* 1001 for _ in range(1001)]
+
+while q:
+    x_screen, x_clip = q.popleft()
+    if x_screen == S:
+        ans = visited[x_screen][x_clip]
+        break
+        
+    arr = [
+        (x_screen, x_screen),
+        (x_screen+x_clip, x_clip),
+        (x_screen-1,x_clip)
+    ]
+    for screen, clip in arr:
+        if 0<=screen<1001 and 0<=clip<1001 and not visited[screen][clip]:
+            q.append((screen,clip)) #현재 화면에 존재하는 이모티콘 수만큼 클립보드에 복사사
+            visited[screen][clip] = visited[x_screen][x_clip]+1
+    
+print(ans)
